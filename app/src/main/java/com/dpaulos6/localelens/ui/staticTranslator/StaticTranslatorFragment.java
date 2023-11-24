26e624b155c2037d56f39a12a9ada8c51c65cbc5
Diogo Trigueiros <121031730+diogotrigueiros@users.noreply.github.com>
2023-11-24 11:21:01 +0000

    24/11/23
    storage

--- app/src/main/java/com/dpaulos6/localelens/ui/staticTranslator/StaticTranslatorFragment.java
+++ app/src/main/java/com/dpaulos6/localelens/ui/staticTranslator/StaticTranslatorFragment.java
@@ -1,19 +1,55 @@
 package com.dpaulos6.localelens.ui.staticTranslator;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
+import com.dpaulos6.localelens.R;
 import com.dpaulos6.localelens.databinding.FragmentStaticTranslatorBinding;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class StaticTranslatorFragment extends Fragment
 {
+
+  ActivityResultLauncher<Intent> fileLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+          result->{
+            Intent res = result.getData();
+            if(res != null && res.getData() != null) {
+              Uri caminhoParaoFicheiro = res.getData();
+              try {
+                InputStream stream = requireActivity().getContentResolver().openInputStream(caminhoParaoFicheiro);
+                Bitmap imagem = BitmapFactory.decodeStream(stream);
+
+                ImageView img = requireActivity().findViewById(R.id.imgView);
+                Glide.with(this).load(imagem).into(img);
+
+              } catch (FileNotFoundException e)
+              {
+                Log.d("MDlog", "No pic");
+              }
+            }
+          });
 
   private FragmentStaticTranslatorBinding binding;
 
@@ -27,10 +63,16 @@ public class StaticTranslatorFragment extends Fragment
     View root = binding.getRoot();
 
 
-
     final TextView textView = binding.textStaticTranslator;
     staticTranslatorViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
     return root;
+  }
+
+  public void pesquisarFicheiro(View view)
+  {
+    Intent inFile = new Intent(Intent.ACTION_PICK);
+    inFile.setType("image/*");
+    fileLauncher.launch(inFile);
   }
 
   @Override
